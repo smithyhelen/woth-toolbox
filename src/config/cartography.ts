2c4ceb19e75806836da6d6661414e599171d2f65
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-10-28 16:10:36 +0000

    Restore tutorial dependencies from commit 2821340

---
+++ src/config/cartography.ts
@@ -0,0 +1,10 @@
+import type { MapType } from 'types/cartography';
+
+// List of all maps available in the application
+export const mapTypes: Array<MapType> = [
+  'africa',
+  'alaska',
+  'idaho',
+  'new-zealand',
+  'transylvania',
+];
