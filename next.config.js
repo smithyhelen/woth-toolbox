bde6598dde9268761f8d35f1a355c0f126c19ad0
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-11-14 14:55:51 +0000

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const nextConfig = {
+  output: 'export',
+  basePath: '/woth-toolbox',
+  assetPrefix: '/woth-toolbox',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
